4855f00b1feb2a72b2a9a4ab66a78a0e8eb437a7
Matthew Surowiec <mp5auc3@gmail.com>
2014-08-04 12:46:43 -0400

    added divider between make and findimage views

--- MPSProfileViewController.m
+++ MPSProfileViewController.m
@@ -36,6 +36,15 @@
 {
     [super viewDidLoad];
     
+    self.navigationItem.title = @"makersfinders";
+    self.navigationController.navigationBar.titleTextAttributes = @{
+                                                                    NSForegroundColorAttributeName: [UIColor whiteColor],
+                                                                    NSFontAttributeName: [UIFont fontWithName:@"NeutraText-BookSC" size:25.0f]
+                                                                    };
+    
+    self.navigationController.navigationBar.translucent = NO;
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:45/255.0 green:62/255.0 blue:81/255.0 alpha:1];
+    
     
     
     
--- MPSViewController.m
+++ MPSViewController.m
@@ -17,6 +17,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.navigationItem.title = @"makersfinders";
+    self.navigationController.navigationBar.titleTextAttributes = @{
+                                                                    NSForegroundColorAttributeName: [UIColor whiteColor],
+                                                                    NSFontAttributeName: [UIFont fontWithName:@"NeutraText-BookSC" size:25.0f]
+                                                                    };
+    
+    self.navigationController.navigationBar.translucent = NO;
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:45/255.0 green:62/255.0 blue:81/255.0 alpha:1];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
